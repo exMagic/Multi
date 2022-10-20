77b5588995bbfa08deb41d4b5b1a825efd1319d1
Maciek <maciek.anuszkiewicz@gmail.com>
2022-10-20 22:53:20 +0200

    <facepalm>

--- src/secrets.h
+++ src/secrets.h
@@ -1,8 +1,8 @@
-#define SECRET_SSID "S T A R B A S E"
-#define SECRET_PASS "Saiminakarasat576!"
-#define SECRET_DEVICE_PASSWORD "onKc6iNij4vhttOoCljA0A"
+#define SECRET_SSID "xxx"
+#define SECRET_PASS "xxx"//yeah I forgot again to not uplod this file :/ cred. are changed allready:)
+#define SECRET_DEVICE_PASSWORD "xxx"
 #define SECRET_MQTT_Broker_server "192.168.68.73"
-#define SECRET_MQTT_USER "mqtt-user"
-#define SECRET_MQTT_PASSWORD "Walizeczka8/"
+#define SECRET_MQTT_USER "xxx"
+#define SECRET_MQTT_PASSWORD "xxx"
 #define SECRET_MQTT_CLIENT_NAME "Powerwall_ESP32OLED"
-#define SECRET_MQTT_PORT 1883
+#define SECRET_MQTT_PORT xxx
