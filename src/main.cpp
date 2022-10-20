e15260b84675dddf1135f2e80199a149052c53e2
Maciej Anuszkiewicz <maciek.anuszkiewicz@gmail.com>
2022-10-20 18:44:42 +0200

    first commit

---
+++ src/main.cpp
@@ -0,0 +1,187 @@
+#include <Arduino.h>
+#define MDASH_APP_NAME "Powerwall"
+#include <mDash.h>
+#include <WiFi.h>
+#include "secrets.h"
+#include "EspMQTTClient.h"
+
+#include <Battery18650Stats.h>
+#define ADC_PIN 15
+Battery18650Stats battery(ADC_PIN);
+
+const int led = 2;
+unsigned long previousMillis = 0;
+const long interval = 500;
+bool ledState = 0;
+
+float xx = 0;
+float x2 = 0;
+
+/// MQTT
+EspMQTTClient client(
+    SECRET_SSID,
+    SECRET_PASS,
+    SECRET_MQTT_Broker_server, // MQTT Broker server ip
+    SECRET_MQTT_USER,          // Can be omitted if not needed
+    SECRET_MQTT_PASSWORD,      // Can be omitted if not needed
+    SECRET_MQTT_CLIENT_NAME,   // Client name that uniquely identify your device
+    SECRET_MQTT_PORT           // The MQTT port, default to 1883. this line can be omitted
+);
+
+#include <TFT_eSPI.h> // Graphics and font library for ST7735 driver chip
+#include <SPI.h>
+TFT_eSPI tft = TFT_eSPI(); // Invoke library, pins defined in User_Setup.h
+
+// const int numReadings = 100;
+
+// int readings[numReadings];      // the readings from the analog input
+// int readIndex = 0;              // the index of the current reading
+// int total = 0;                  // the running total
+// int average = 0;                // the average
+// int inputPin = 13;
+
+// This function is called once everything is connected (Wifi and MQTT)
+// WARNING : YOU MUST IMPLEMENT IT IF YOU USE EspMQTTClient
+void onConnectionEstablished()
+{
+  // client.subscribe("powerwall", [](const String &payload) {
+  //   Serial.println(payload);
+  // });
+  client.publish("powerwall", "//__START__//"); // You can activate the retain flag by setting the third parameter to true
+}
+
+//#include "esp_adc_cal.h"
+
+#define AN_Pot1 13
+#define FILTER_LEN 100
+
+uint32_t AN_Pot1_Buffer[FILTER_LEN] = {0};
+int AN_Pot1_i = 0;
+int AN_Pot1_Raw = 0;
+int AN_Pot1_Filtered = 0;
+
+uint32_t readADC_Avg(int ADC_Raw)
+{
+  int i = 0;
+  uint32_t Sum = 0;
+
+  AN_Pot1_Buffer[AN_Pot1_i++] = ADC_Raw;
+  if (AN_Pot1_i == FILTER_LEN)
+  {
+    AN_Pot1_i = 0;
+  }
+  for (i = 0; i < FILTER_LEN; i++)
+  {
+    Sum += AN_Pot1_Buffer[i];
+  }
+  return (Sum / FILTER_LEN);
+}
+
+// int vref = 1100;
+// void showVoltage()
+// {
+
+//   static uint64_t timeStamp = 0;
+//   if (millis() - timeStamp > 1000)
+//   {
+//     timeStamp = millis();
+//     uint16_t v = analogRead(ADC_PIN);
+//     float battery_voltage = ((float)v / 4095.0) * 2.0 * 3.3 * (vref / 1000.0);
+//     String voltage = "Voltage :" + String(battery_voltage) + "V";
+//     Serial.println(voltage);
+
+//     tft.fillScreen(TFT_BLACK);
+//     tft.setTextColor(TFT_WHITE, TFT_BLACK);
+//     tft.setTextSize(1);
+//     tft.setCursor(0, 0, 2);
+//     tft.println(voltage);
+
+//     // // Set the font colour to be yellow with no background, set to font 7
+//     // tft.setTextColor(TFT_YELLOW); tft.setTextFont(2);
+//     // tft.println(1234.56);
+
+//     // // Set the font colour to be red with black background, set to font 4
+//     // tft.setTextColor(TFT_RED,TFT_BLACK);    tft.setTextFont(4);
+//     // tft.println((long)3735928559, HEX); // Should print DEADBEEF
+
+//     // // Set the font colour to be green with black background, set to font 2
+//     // tft.setTextColor(TFT_GREEN,TFT_BLACK);
+//     // tft.setTextFont(2);
+//     // tft.println("Groop");
+
+//     // // Test some print formatting functions
+//     // float fnumber = 123.45;
+//     //  // Set the font colour to be blue with no background, set to font 2
+//     // tft.setTextColor(TFT_BLUE);    tft.setTextFont(2);
+//     // tft.print("Float = "); tft.println(fnumber);           // Print floating point number
+//     // tft.print("Binary = "); tft.println((int)fnumber, BIN); // Print as integer value in binary
+//     // tft.print("Hexadecimal = "); tft.println((int)fnumber, HEX); // Print as integer number in Hexadecimal
+
+//     while (1)
+//       yield(); // We must yield() to stop a watchdog timeout.
+//   }
+// }
+
+void setup()
+{
+
+  tft.init();
+  tft.setRotation(1);
+
+  // analogSetPinAttenuation(AN_Pot1, ADC_0db);
+  Serial.begin(9600);
+  // WiFi.begin(SECRET_SSID, SECRET_PASS);
+  // mDashBegin(SECRET_DEVICE_PASSWORD);
+  pinMode(led, OUTPUT);
+  // client.enableDebuggingMessages();                              // Enable debugging messages sent to serial output
+  // client.enableLastWillMessage("powerwall", "I am going offline"); // You can activate the retain flag by setting the third parameter to true
+
+  // for (int thisReading = 0; thisReading < numReadings; thisReading++) {
+  //   readings[thisReading] = 0;
+  // }
+
+  // // initialize all the readings to 0:
+  // for (int thisReading = 0; thisReading < numReadings; thisReading++) {
+  //   readings[thisReading] = 0;
+  // }
+}
+
+void loop()
+{
+
+  // int vref = 3300;
+  // uint16_t v = analogRead(ADC_PIN);
+  // uint16_t v2 = v-107;
+  // float battery_voltage = ((float)v2 / 4095.0) * 2.0 * 3.3 * (vref / 1000.0);
+  // String voltage = "Voltage :" + String(battery_voltage) + "V";
+
+  AN_Pot1_Raw = analogRead(ADC_PIN);
+  AN_Pot1_Filtered = readADC_Avg(AN_Pot1_Raw);
+
+  unsigned long currentMillis = millis();
+  if (currentMillis - previousMillis >= interval)
+  {
+    previousMillis = currentMillis;
+    ledState = not(ledState);
+    digitalWrite(led, ledState);
+
+    tft.fillScreen(TFT_BLACK);
+    tft.setTextColor(TFT_WHITE, TFT_BLACK);
+    tft.setTextSize(1);
+    tft.setCursor(0, 0, 2);
+
+    double f = (3150.0 - 107.0) / 4095.0;
+    float c = 107 - (int)AN_Pot1_Filtered;
+    float n = c / f * -1;
+
+    float s0 = (3150.0-107.0)/4095.0;
+    float s1 = 107+(s0 * AN_Pot1_Filtered);
+
+    tft.println(s0);
+    tft.println(s1);
+    tft.println(AN_Pot1_Filtered);
+
+    Serial.println(s1);
+    Serial.println(AN_Pot1_Filtered);
+  }
+}
---
+++ src/secrets.h
@@ -0,0 +1,8 @@
+#define SECRET_SSID "S T A R B A S E"
+#define SECRET_PASS "Saiminakarasat576!"
+#define SECRET_DEVICE_PASSWORD "onKc6iNij4vhttOoCljA0A"
+#define SECRET_MQTT_Broker_server "192.168.68.73"
+#define SECRET_MQTT_USER "mqtt-user"
+#define SECRET_MQTT_PASSWORD "Walizeczka8/"
+#define SECRET_MQTT_CLIENT_NAME "Powerwall_ESP32OLED"
+#define SECRET_MQTT_PORT 1883
